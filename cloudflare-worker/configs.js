feab56541ad2b8ea29cf17cda1982bdf74ceda7c
Lee Ingram <lee@soylentcola.com>
2024-02-18 08:46:51 -0800

    Manually ran render-configs.py

--- cloudflare-worker/configs.js
+++ cloudflare-worker/configs.js
@@ -1,68 +1,66 @@
 export const CONFIGS = {
-  "emotional-suppo": {
-    "recordName": "emotional-suppo",
-    "displayName": "Emotional Support Pets",
-    "description": "Cute animals feed",
+  "horror-feed": {
+    "recordName": "horror-feed",
+    "displayName": "Horror \u00f0\u0178\u2019\u20ac\u00f0\u0178\u201d\u00aa",
+    "description": "A feed for all things horror",
     "searchTerms": [
-      "at://did:plc:ozppa2bsq6bdnajyweoir2i2/app.bsky.feed.post/3jxju2wwap22e",
-      "cats",
-      "dogs",
-      "penguins",
-      "red pandas",
-      "quokkas"
+      "horror game",
+      "horror film",
+      "horror movie",
+      "horror novel",
+      "horror book"
     ],
     "denyList": [],
     "safeMode": true,
-    "avatar": "avatar.png",
+    "avatar": "horror_avatar.png",
     "isEnabled": true
   },
-  "bluesky-app": {
-    "recordName": "bluesky-app",
-    "isEnabled": false,
-    "displayName": "Bluesky App",
-    "description": "Updates about the app",
+  "budlight-skeets": {
+    "recordName": "budlight-skeets",
+    "isEnabled": true,
+    "displayName": "Bud Light Skeets",
+    "description": "Skeets that mention Bud Light",
     "searchTerms": [
-      "at://did:plc:z72i7hdynmk6r22z27h6tvur",
-      "at://did:plc:ewvi7nxzyoun6zhxrhs64oiz"
+      "bud light"
     ],
+    "denyList": [],
     "safeMode": true,
-    "avatar": "configs/avatar2.png"
+    "avatar": "configs\\budlight.png"
   },
-  "science-emojis": {
-    "recordName": "science-emojis",
-    "isEnabled": false,
-    "displayName": "Science Emojis",
-    "description": "Posts with \ud83e\uddea\ud83e\udd7c\ud83d\udd2d",
+  "coffee-skeets": {
+    "recordName": "coffee-skeets",
+    "isEnabled": true,
+    "displayName": "Coffee Skeets",
+    "description": "Posts about coffee",
     "searchTerms": [
-      "\ud83e\uddea",
-      "\ud83e\udd7c",
-      "\ud83d\udd2d"
+      "coffee"
     ],
+    "denyList": [],
     "safeMode": true,
-    "avatar": "configs/avatar2.png"
+    "avatar": "configs\\coffee.png"
   },
-  "gaming-emojis": {
-    "recordName": "gaming-emojis",
-    "isEnabled": false,
-    "displayName": "Gaming Emojis",
-    "description": "Posts with \ud83d\udc7e\ud83c\udfae\ud83d\udd79\ufe0f",
+  "vrchat-skeets": {
+    "recordName": "vrchat-skeets",
+    "isEnabled": true,
+    "displayName": "VRChat Skeets",
+    "description": "Posts about VRChat",
     "searchTerms": [
-      "\ud83d\udc7e",
-      "\ud83c\udfae",
-      "\ud83d\udd79\ufe0f"
+      "vrchat"
     ],
+    "denyList": [],
     "safeMode": true,
-    "avatar": "configs/avatar2.png"
+    "avatar": "configs\\vrchat.png"
   },
-  "basketball-emoj": {
-    "recordName": "basketball-emoj",
-    "isEnabled": false,
-    "displayName": "Basketball Emojis",
-    "description": "Posts with \ud83c\udfc0",
+  "weed-time": {
+    "recordName": "weed-time",
+    "isEnabled": true,
+    "displayName": "Weed Time! \u00f0\u0178\u0152\u00b1",
+    "description": "Every skeet that mentions weed time",
     "searchTerms": [
-      "\ud83c\udfc0"
+      "weed time"
     ],
+    "denyList": [],
     "safeMode": true,
-    "avatar": "configs/avatar2.png"
+    "avatar": "configs\\weed_feed.png"
   }
 }
